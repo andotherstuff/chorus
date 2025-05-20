403dec76017cb1f0e9464e80fd23ab4fbf0201ef
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2025-05-20 12:03:26 +0200

    fix router spacing

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,25 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Settings from "@/pages/Settings";
-import CreateGroup from "./pages/CreateGroup";
-import GroupDetail from "./pages/GroupDetail";
-import Groups from "./pages/Groups";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import Settings from '@/pages/Settings';
+import CreateGroup from './pages/CreateGroup';
+import GroupDetail from './pages/GroupDetail';
+import Groups from './pages/Groups';
+import Index from './pages/Index';
+import NotFound from './pages/NotFound';
 
 export function AppRouter() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Index />} />
-				<Route path="/groups" element={<Groups />} />
-				<Route path="/group/:groupId" element={<GroupDetail />} />
-				<Route path="/create-group" element={<CreateGroup />} />
-				<Route path="/settings" element={<Settings />} />
-				{/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Index />} />
+        <Route path='/groups' element={<Groups />} />
+        <Route path='/group/:groupId' element={<GroupDetail />} />
+        <Route path='/create-group' element={<CreateGroup />} />
+        <Route path='/settings' element={<Settings />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default AppRouter;
